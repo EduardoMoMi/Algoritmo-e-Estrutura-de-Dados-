d96d715f266f31b58d7f9738b221348eae741c5f
Eduardo M. de Miranda <dudumegabriel@gmail.com>
2026-04-08 14:51:01 -0300

    Salvando todos os códigos para modificação

---
+++ 1° Lista de Exercícios/Josephus.java
@@ -0,0 +1,35 @@
+public class Josephus {
+    public static void main(String[] args) {
+        int N = 6;    // numero de pessoas
+        int M = 3;    // salto (elimina a cada M contagens)
+
+        // --- Passo 1: Montar a lista circular ---
+        Node primeiro = new Node(1);
+        Node atual = primeiro;
+        for (int i = 2; i <= N; i++) {
+            // TODO: crie um novo Node com valor i
+            // TODO: faca atual.next = novo
+            // TODO: avance atual para novo
+        }
+        // TODO: faca atual.next = primeiro (fecha o circulo!)
+
+        // --- Passo 2: Simular a eliminacao ---
+        Node anterior = atual;    // o no "antes" do atual
+        atual = primeiro;
+
+        System.out.println("Ordem de eliminacao:");
+        while (atual.next != atual) {    // enquanto houver mais de 1
+
+            // TODO: avance (anterior, atual) por M-1 posicoes
+            //       Isto e, repita M-1 vezes:
+            //           anterior = atual;
+            //           atual = atual.next;
+
+            // TODO: elimine 'atual' fazendo anterior.next = atual.next
+            // TODO: imprima o eliminado: atual.item
+            // TODO: avance atual para anterior.next
+        }
+        System.out.println("\nSobrevivente: " + atual.item);
+
+    }
+}
---
+++ 1° Lista de Exercícios/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    public Object item;    // dado armazenado (generico)
+    public Node next;      // referencia ao proximo no
+
+    // TODO: crie um construtor Node(Object item) que
+    //       inicializa this.item e coloca next = null
+
+    // TODO: crie um segundo construtor Node(Object item, Node next)
+    //       que permite ja encadear na criacao
+}
